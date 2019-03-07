ce77ab66aee7695d17570a4046dc984ebab4f0b0
avell21 <abeltyr@gmail.com>
2019-03-07 15:08:35 +0300

    added beuty

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import Vuetify from "vuetify";
+import App from "./App.vue";
+import "vuetify/dist/vuetify.css";
+import "@mdi/font/css/materialdesignicons.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+Vue.use(Vuetify);
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
