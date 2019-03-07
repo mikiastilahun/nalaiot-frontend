1743089c13b8a302b2e28854070ca61c97dd80f9
MikeAbebe <mikiastilahun@gmail.com>
2019-03-07 15:12:16 +0300

    finished logic

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" :width="value*20" :height="value*20" src="./assets/logo.png">
+    <h1>{{value}} cm</h1>
+    <h1> this is a text </h1>
+    <div id="green">
+       <button v-on:click="changeGreen">Switch</button>
+    </div>
+
+    <div id="yellow">
+       <button v-on:click="changeYellow">Switch</button>
+    </div>
+
+    <div id="red">
+       <button v-on:click="changeRed">Switch</button>
+    </div>
+   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+    data:()=>{
+      return{
+        value: 1,
+        greenState: 1,
+        yellowState: 1,
+        redState: 1
+      }
+    },
+    methods:{
+      changeGreen:function(){
+      this.$socket.emit("switchGreen",this.greenState)
+      this.greenState = !this.greenState
+      },
+      changeYellow: function(){
+      this.$socket.emit("switchYellow",this.yellowState)
+      this.yellowState = !this.yellowState
+      },
+      changeRed:function(){
+      this.$socket.emit("switchRed",this.redState)
+      this.redState = !this.redState
+      }
+    },
+    sockets: {
+        sensorData: function (data){
+            this.value = parseInt(data)
+        }
+    },
 }
 </script>
 
-<style>
+<style scoped>
+#green{
+  background-color: green;
+  width: 50;
+  margin: 20;
+}
+#yellow{
+  background-color: yellow;
+  width: 50;
+  margin: 20
+}
+#red{
+  background-color: red;
+  width: 50;
+  margin: 20
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueSocketIO from "vue-socket.io"
 
 Vue.config.productionTip = false
-
+Vue.use(new VueSocketIO({
+    debug:true,
+    connection: "172.20.10.2:3001",
+}))
 new Vue({
   render: h => h(App),
 }).$mount('#app')
